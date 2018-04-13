13fd8027b903b9271942d2f748964ca2ed483509
侯森 <1476373943@qq.com>
2018-04-13 13:28:24 +0800

    初始化

---
+++ src/main/java/com/hs/controller/LocalDateTime.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.hs.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+*
+*@author HS <br>
+*2018年4月13日 上午10:55:16
+*/
+@RestController
+public class LocalDateTime {
+    public String localDate(){
+        
+        return "holle";
+    }
+    
+}
